d362beb358cea162237c0f6ce061ce27320064d6
Ankur Tandulwadkar <ankur.tandulwadkar@voidstarindia.com>
2020-07-15 20:22:23 +0530

    Color changing code
    2 code

---
+++ 01-Win32/04-ColoringAMessage/04-ColoringAMessage.cpp
@@ -0,0 +1,70 @@
+#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR csCmd, int iShow)
+{
+	WNDCLASSEX wndClass;
+	MSG msg;
+	HWND hwnd;
+	TCHAR szAppName[] = TEXT("HelloWorld");
+
+	wndClass.cbClsExtra = 0;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.lpszClassName = szAppName;
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClassEx(&wndClass))
+	{
+		MessageBox(NULL, TEXT("Error in window creation"), TEXT("Error"), MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindow(szAppName, TEXT("Hello World"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	UpdateWindow(hwnd);
+	ShowWindow(hwnd, iShow);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	RECT rc;
+	TCHAR str[] = TEXT("Hello World");
+	PAINTSTRUCT ps;
+
+	switch (msg)
+	{
+	case WM_PAINT:
+		GetClientRect(hwnd, &rc);
+		hdc = BeginPaint(hwnd, &ps);
+		SetTextColor(hdc, RGB(0, 255, 0));
+		SetBkColor(hdc, RGB(0, 0, 0));
+		DrawText(hdc, str, -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		EndPaint(hwnd, &ps);
+
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
